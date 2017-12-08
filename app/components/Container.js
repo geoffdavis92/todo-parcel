a8c4945ba5d432b60f727c11daf4c612d9f45d0f
Geoff Davis <gdavis@archermalmo.com>
2017-12-08 11:17:09 -0600

    add container code

--- app/components/Container.js
+++ app/components/Container.js
@@ -2,4 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../utils/settings";
 
-const Container = styled.main``;
+const Container = styled.main`
+  border: 1px solid ${theme.colors.border};
+  padding: 2.5em;
+`;
